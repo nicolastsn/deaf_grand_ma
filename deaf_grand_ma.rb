4603ab9a31bcda7d58d73cf5a2368b039358fd2f
seimori <issei.morimoto@gmail.com>
2017-08-09 22:41:39 +0200

    Finishedgaga!

--- deaf_grand_ma.rb
+++ deaf_grand_ma.rb
@@ -1,41 +1,38 @@
-require 'pry'
-
 class Grandma
   def initialize
-     @ready_to_quit = false
-     @input = gets
   end
 
   def talk
-    puts "HEY KIDS!"
+    puts "HEY KID!"
+    already_said_it = false
+    @ready_to_quit = false
 
     until @ready_to_quit
-
-      if downcase_checking? 
+      @input = gets.chomp
+      if downcase_checking?
         puts "SPEAK UP KID !"
-      elsif !downcase_checking? && !@input.nil? && @input != "GOODBYE!"
+      elsif !downcase_checking? && @input != '' && @input != "GOODBYE!"
         puts "NO, NOT SINCE 1946!"
-      elsif input == "GOODBYE!" && count == 
-
-          
-          
-
-
+      elsif @input == "GOODBYE!" && !already_said_it
+        already_said_it = true
+        puts "LEAVING SO SOON?"
+      elsif @input == "GOODBYE!" && already_said_it
+        @ready_to_quit = true
+      else
+        puts "WHAT?!"
+      end
     end
-
     puts "LATER SKATER !"
   end
 
 
 
-
-  #détermine si il y a une minuscule
+  #détermine s'il y a une minuscule
   def downcase_checking?
     sentence = @input.split('')
     sentence.any? {|val| /[a-z]/ =~ val}
   end
 end
 
-binding.pry
-
-puts "end of code"
+mamie = Grandma.new
+mamie.talk
